354a5cb935a0c0a53012cd220d34eec3330a1b8a
Carlos Azañon <66433558+Cjazanon@users.noreply.github.com>
2020-08-12 16:44:09 -0600

    subida de archivos
    los archivos se subieron al host

---
+++ js.js
@@ -0,0 +1,25 @@
+﻿var grupoTarjeta1 = ["🐶", "🐺", "🐱","🐵","💥", "📕", "🐷","🐪","💻","🦈","🐘","🐹","🐸","🐨","🐼","🎃","🐣","🦀"];
+var grupoTarjeta2 = ["🐶", "🐺", "🐱","🐵","💥", "📕", "🐷","🐪","💻","🦈","🐘","🐹","🐸","🐨","🐼","🎃","🐣","🦀"];
+
+var todasLasTarjetas = grupoTarjeta1.concat(grupoTarjeta2);
+
+function crearTajetas() {
+  var mesa = document.querySelector("#mesa");
+
+  todasLasTarjetas.forEach(function mensaje(Element) {
+    var tarjeta = document.createElement("div");
+
+
+    tarjeta.innerHTML = "<p class='ico'>" + Element + "</p>";
+
+    mesa.appendChild(tarjeta);
+
+  });
+}
+
+crearTajetas();
+
+
+
+
+/*<div class="tarjeta">🐶</div>*/
